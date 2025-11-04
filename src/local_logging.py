d501035a487b7839307684aa3423a9ae42645c31
kaylamckenzie <kmckenzie82@bethelu.edu>
2025-11-04 14:48:42 -0600

    Added Logger

---
+++ src/local_logging.py
@@ -0,0 +1,22 @@
+import logging
+import os
+from datetime import datetime
+
+def get_logger():
+
+    log_dir = "logs"
+    os.makedirs(log_dir, exist_ok=True)
+
+    log_filename = f"{log_dir}/pipeline_{datetime.now().strftime('%Y%m%d')}.log"
+    
+    logging.basicConfig(
+        filename=log_filename,
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(message)s",
+        handlers=[
+            logging.StreamHandler(),
+            logging.FileHandler(log_filename)
+        ]
+    )
+    logger = logging.getLogger()
+    return logger
--- src/main.py
+++ src/main.py
@@ -2,6 +2,9 @@ import database_service as ds
 
 def main():
     student_data = ds.get_student_score_info()
+    student_demographics = ss.get_students()
+
+    
 
 if __name__ == "__main__":
     main()
